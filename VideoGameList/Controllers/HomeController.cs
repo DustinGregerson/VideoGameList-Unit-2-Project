f902b4030d0f8aa9db3b86229553ddaaec7d4ce8
DustinGregerson <dustin.gregerson@southeasttech.edu>
2023-10-15 17:38:18 -0500

    Added comments.

--- VideoGameList/Controllers/HomeController.cs
+++ VideoGameList/Controllers/HomeController.cs
@@ -3,6 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using VideoGameList.Models;
 
+
+//Author Dustin Gregerson
+//Date 10/15/2023
+//Description:
+/*
+ * This is the home controller for the site. It passes the index page to the browser
+ * it also compiles a list of video game models that are derived from the video games data table
+ */
 namespace VideoGameList.Controllers
 {
     public class HomeController : Controller
@@ -13,6 +21,8 @@ namespace VideoGameList.Controllers
         {
             context = ctx;
         }
+        //when this method is called every record in the videoGames table will be stored in a videoGame model list
+        //this list is then passed to the index page.
         public IActionResult Index()
         {
             var videoGames = context.videoGames.ToList();
--- VideoGameList/Controllers/VideoGameController.cs
+++ VideoGameList/Controllers/VideoGameController.cs
@@ -1,6 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VideoGameList.Models;
+//Author Dustin Gregerson
+//Date 10/15/2023
+//Description:
+/*
+ * Directs the traffic from the index page to the edit or delete pages depending on what button is clicked.
+ * Each button is set to target the correct page and if required carries the model’s id so the data base will
+ * know which record to update or delete from the Video Games table.
 
+ */
 namespace VideoGameList.Controllers
 {
     public class VideoGameController : Controller
@@ -12,12 +20,14 @@ namespace VideoGameList.Controllers
             context = ctx;
         }
 
+        //Creates a new video game and increments the id by 1 for the new record
         [HttpGet]
         public IActionResult Add()
         {
             ViewBag.Action = "Add";
             return View("Edit", new videoGame());
         }
+        //Gets the id for the Video Game from the index page to alter the record in question in the edit page.
         [HttpGet]
         public IActionResult Edit(int id)
         {
@@ -25,6 +35,8 @@ namespace VideoGameList.Controllers
             var videoGame = context.videoGames.Find(id);
             return View(videoGame);
         }
+        //When passed a video game if that games id is not equal 0 a new game is created
+        //if that games id is not equal to 0 then the game is updated
         [HttpPost]
         public IActionResult Edit(videoGame videoGame)
         {
@@ -43,13 +55,14 @@ namespace VideoGameList.Controllers
                 return View(videoGame);
             }
         }
+        //Returns the delete view
         [HttpGet]
         public IActionResult Delete(int id)
         {
             var videoGame = context.videoGames.Find(id);
             return View(videoGame);
         }
-
+        //Findes the video game in the video games table and deletes it from the data base
         [HttpPost]
         public IActionResult Delete(videoGame videoGame)
         {
