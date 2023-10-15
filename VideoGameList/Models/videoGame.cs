0ed7432445a2d1799b34dc7860df68fc7bd2a2c8
DustinGregerson <dustin.gregerson@southeasttech.edu>
2023-10-15 03:14:21 -0500

    Added video game and video game context.

--- VideoGameList/Models/videoGame.cs
+++ VideoGameList/Models/videoGame.cs
@@ -1,7 +1,16 @@
-﻿namespace VideoGameList.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace VideoGameList.Models
 {
     public class videoGame
     {
+        public int videoGameId { get; set; }
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string? name { get; set; }
+        [Required(ErrorMessage = "Please enter favorite character.")]
+        public string? favoriteCharacter { get; set; }
+        [Required(ErrorMessage = "Please enter favorite character.")]
+        public bool? wouldPlayAgain { get; set; }
+        [Required(ErrorMessage = "Please enter yes or no.")]
 
     }
 }
---
+++ VideoGameList/Models/videoGameContext.cs
@@ -0,0 +1,45 @@
+﻿using Humanizer.Localisation;
+using Microsoft.EntityFrameworkCore;
+
+namespace VideoGameList.Models
+{
+    public class videoGameContext : DbContext
+    {
+        public videoGameContext()
+        {
+
+        }
+        public videoGameContext(DbContextOptions<videoGameContext> options)
+            : base(options) { }
+
+
+        public DbSet<videoGame> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<videoGame>().HasData(
+            new videoGame
+            {
+                videoGameId = 1,
+                name = "Castlevania Symphony of the Night",
+                favoriteCharacter = "alucard",
+                wouldPlayAgain = true
+            },
+             new videoGame
+             {
+                 videoGameId = 1,
+                 name = "Factorio",
+                 favoriteCharacter = "player",
+                 wouldPlayAgain = true
+             },
+             new videoGame
+             {
+                 videoGameId = 1,
+                 name = "satisfactory",
+                 favoriteCharacter = "Protagonist",
+                 wouldPlayAgain = true
+             }
+            ); ;
+        }
+    }
+}
